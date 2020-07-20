d2639e57ed9b90880c5ef0625e0f35268d3e1cdb
Bogdan <bogdan7morozyuk@gmail.com>
2020-07-20 10:08:11 +0300

    chore(server): mount express-graphql as a route handler

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+
+const schema = require('./schema');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+app.listen(
+  port, 
+  () => {
+    console.log(`Server started on port ${port}`)
+  }
+);
