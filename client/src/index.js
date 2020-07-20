501bfd56ac4a321286dc136336ecc191496e88b8
Bogdan <bogdan7morozyuk@gmail.com>
2020-07-20 10:43:08 +0300

    chore: add bootstrap-lux

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 import App from 'App';
 import * as serviceWorker from 'serviceWorker';
 
+import 'assets/bootstrap-lux.min.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
