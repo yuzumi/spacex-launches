5c7d7954d1792eaecd9186e38bc8430671c2599f
Bogdan <bogdan7morozyuk@gmail.com>
2020-07-20 11:26:36 +0300

    chore(server): allow cross-origin

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 
@@ -6,6 +7,8 @@ const schema = require('./schema');
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.use(
   '/graphql',
